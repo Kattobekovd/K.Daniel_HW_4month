5222d772634e86fde8a862baeaf98406fd65495b
Kattobekovd <Kattobekovd@gmail.com>
2024-09-17 23:14:03 +0600

    dz4

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create_book/',views.creat_book_view,name='add_books'),
     path('about_me/', views.about_me_view),
     path('about_my_friend/', views.about_my_friend_view),
     path('this_time/', views.this_time_view),
     path('post_list/', views.post_list),
+    path('post_list/delete/', views.Book_list_delete_view,name='delete_books' ),
+    path('post_list_edit/', views.Book_list_edit_view,name='edit_books' ),
     path('post_list/<int:id>/', views.post_detail),
+    path('post_list/<int:id>/delete/', views.Book_drop_view),
+    path('post_list/<int:id>/update/', views.update_book_view)
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,65 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
-from . import models
+from . import models, forms
+
+
+#Редактирование crud, получение список постов
+def Book_list_edit_view(request):
+    if request.method == 'GET':
+        post_object = models.Book.objects.all()
+        return render(
+            request,
+            'crud/book_list_edit.html',
+            {'post_object': post_object}
+        )
+#ЗДесь функция на редактирование обявлений из списка
+def update_book_view(request, id):
+    post_id = get_object_or_404(models.Book, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(request.POST, instance=post_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Данные успешно Отредактирована!! <a href = /post_list/ > На список обьявлений книг </a>')
+    else:
+        form = forms.BookForm(instance=post_id)
+    return render(
+        request,
+        template_name='crud/book_update.html',
+        context={'form': form, 'post_id': post_id}
+    )
+
+#Удаление crud, здесь мы получаем листы
+def Book_list_delete_view(request):
+    if request.method == 'GET':
+        post_object = models.Book.objects.all()
+        return render(
+            request,
+            'crud/book_list_delete.html',
+            {'post_object': post_object}
+        )
+
+# здесь функция на удаление постов из листа
+def Book_drop_view(request, id):
+    post_id = get_object_or_404(models.Book, id=id)
+    post_id.delete()
+    return HttpResponse('Данные успешно Удалены!! <a href = /post_list/ > На список обьявлений книг </a>')
+
+
+#Добавление crud
+def creat_book_view(request):
+    if request.method == 'POST':
+        form = forms.BookForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Данные успешно отправлены!! <a href = /post_list/ > На список обьявлений книг </a>')
+    else:
+        form = forms.BookForm()
+    return render(
+        request,
+        'crud/create_book.html',
+        {'form':form}
+    )
 
 
 def post_detail(request, id):
